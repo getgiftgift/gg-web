cacb0041e7afc202f9ac6b9878d962c6d01d3b33
David Hamilton <dham04ster@gmail.com>
2015-04-16 10:43:17 -0500

    Geocoder config.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,9 +1,7 @@
 # Heroku requires an http proxy to make API calls.
 # Set geocoder to use quotaguard.
 
-if Rails.env.production?
-  Geocoder.configure(
-    :http_proxy => ENV['QUOTAGUARD_URL'],
-    :timeout => 5
-  )
-end
+Geocoder.configure(
+  :http_proxy => ENV['QUOTAGUARD_URL'],
+  :timeout => 5
+)
