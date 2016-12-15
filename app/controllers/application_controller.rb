66f1476f605aba94dde41e52a3197d6fc9f500e0
Joshua Smith <jds5x4@gmail.com>
2016-12-14 20:53:01 -0500

    Fix layouts for default routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -79,14 +79,14 @@ class ApplicationController < ActionController::Base
   def verify_login_and_birthday
     if customer_logged_in?
       birthday = current_user.adjusted_birthday
-      return render 'customer_enter_birthday' if birthday.blank?
-      return render 'customer_enter_location' if current_user.location.blank?
+      return render 'birthday_deals/customer_enter_birthday' if birthday.blank?
+      return render 'birthday_deals/customer_enter_location' if current_user.location.blank?
       unless current_user.eligible_for_birthday_deals?
-        return render 'index_not_your_birthday'
+        return render 'birthday_deals/index_not_your_birthday'
       end
     else
       session[:return_to] = birthday_deals_path
-      return render 'index_customer_login'
+      return render 'birthday_deals/index_customer_login'
     end
   end
 end
