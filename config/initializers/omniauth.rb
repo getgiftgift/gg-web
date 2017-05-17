4d557100614452ec756fe3fb83b4aa718d53e565
Joshua Smith <jds5x4@gmail.com>
2017-05-17 19:42:08 -0400

    Update omniauth config for facebook api 2.3

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], :scope => 'email,user_birthday,read_stream' #, :display => 'popup'
+  provider :facebook,
+    ENV['FACEBOOK_APP_ID'],
+    ENV['FACEBOOK_APP_SECRET'],
+    scope: 'email,user_birthday,read_stream',
+    token_params: { parse: :json }
+     #, :display => 'popup'
+    }
 end
