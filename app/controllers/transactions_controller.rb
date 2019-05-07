e23e23dcb47b02ec49879e64745a01d771183b5e
Joshua Smith <jds5x4@gmail.com>
2019-05-06 22:05:47 -0500

    fix: don't take client side transaction fee.
    * Formatting fixes to 2 spaces.
    * Missing braintree scripts for new payment method
    * Disable additional form spot

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,83 +1,86 @@
+# frozen_string_literal: true
+
 class TransactionsController < ApplicationController
-	skip_filter :verify_login_and_birthday
-	
-	helper_method :voucher, :party, :saved_user_payment_token
+  skip_filter :verify_login_and_birthday
 
-	def new
-		if saved_user_payment_token
-			@payment_method = gateway.payment_method.find(saved_user_payment_token)
-		else
-			redirect_to [:new_payment_method, voucher] and return
-		end
-		@transaction = Transaction.new
-	end
+  helper_method :voucher, :party, :saved_user_payment_token
 
-	def create
-		if params[:payment] == 'new'
-			redirect_to [:new_payment_method, voucher] and return
-		end
-		amount = params[:amount]
-		if params[:payment] == 'existing' 
-			result = gateway.transaction.sale(
-				amount: amount,
-				payment_method_token: params[:payment_method_token],
-				options: {
-					submit_for_settlement: true
-				}
-			)
-		else
-			result = gateway.transaction.sale(
-				amount: amount.to_s,
-				payment_method_nonce: params[:payment_method_nonce],
-				options: {
-					submit_for_settlement: true,
-					store_in_vault_on_success: true
-				}
-			)
-		end
-		if result.success?
-			response = result.transaction
-			flash[:success] = 'Payment processed successfullly!'
-			current_user.update(payment_token: response.credit_card_details.token)
-			Transaction.create( 
-				transaction_id: response.id, 
-				amount: Monetize.parse(response.amount),
-				voucher: voucher,
-				birthday_party: party
-			)
-			voucher.make_redeemable!
-			redirect_to voucher
-		else
-			flash[:info] = 'There was an issue try again'
-			render :new
-		end
+  TRANSACTION_FEE = 0.99
 
-		
-	end
+  def new
+    if saved_user_payment_token
+      @payment_method = gateway.payment_method.find(saved_user_payment_token)
+    else
+      redirect_to([:new_payment_method, voucher]) && return
+    end
+    @transaction = Transaction.new
+  end
 
-	def new_payment_method
-		@client_token = gateway.client_token.generate
-	end
+  def create
+    if params[:payment] == 'new'
+      redirect_to([:new_payment_method, voucher]) && return
+    end
 
-	private
-		def gateway
-			@gateway ||= Braintree::Gateway.new(
-				:environment => :sandbox,
-				:merchant_id => ENV["BRAINTREE_MERCHANT_ID"],
-				:public_key => ENV["BRAINTREE_PUBLIC_KEY"],
-				:private_key => ENV["BRAINTREE_PRIVATE_KEY"],
-			)
-		end
-		
-		def voucher
-			@voucher ||= BirthdayDealVoucher.find params[:id]
-		end
+    result = if params[:payment] == 'existing'
+               gateway.transaction.sale(
+                 amount: TRANSACTION_FEE,
+                 payment_method_token: params[:payment_method_token],
+                 options: {
+                   submit_for_settlement: true
+                 }
+               )
+             else
+               gateway.transaction.sale(
+                 amount: TRANSACTION_FEE.to_s,
+                 payment_method_nonce: params[:payment_method_nonce],
+                 options: {
+                   submit_for_settlement: true,
+                   store_in_vault_on_success: true
+                 }
+               )
+    end
+    if result.success?
+      response = result.transaction
+      flash[:success] = 'Payment processed successfullly!'
+      current_user.update(payment_token: response.credit_card_details.token)
+      Transaction.create(
+        transaction_id: response.id,
+        amount: Monetize.parse(response.amount),
+        voucher: voucher,
+        birthday_party: party
+      )
+      voucher.make_redeemable!
+      redirect_to voucher
+    else
+      flash[:info] = 'There was an issue try again'
+      render :new
+    end
+  end
 
-		def saved_user_payment_token
-			@saved_user_payment_token ||= current_user.payment_token
-		end
+  def new_payment_method
+    @client_token = gateway.client_token.generate
+  end
 
-		def party
-			@party ||= voucher.birthday_party
-		end
+  private
+
+  def gateway
+    @gateway ||= Braintree::Gateway.new(
+      environment: :sandbox,
+      merchant_id: ENV['BRAINTREE_MERCHANT_ID'],
+      public_key: ENV['BRAINTREE_PUBLIC_KEY'],
+      private_key: ENV['BRAINTREE_PRIVATE_KEY']
+    )
+  end
+
+  def voucher
+    @voucher ||= BirthdayDealVoucher.find params[:id]
+  end
+
+  def saved_user_payment_token
+    @saved_user_payment_token ||= current_user.payment_token
+  end
+
+  def party
+    @party ||= voucher.birthday_party
+  end
 end
